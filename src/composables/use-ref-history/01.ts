c2adeb9fd95fbaf90296a92ec231181440e4e17e
pei-pay <gleektennis@icloud.com>
2025-01-10 23:35:17 +0900

    update: useRefHistory 01

--- src/composables/use-ref-history/01.ts
+++ src/composables/use-ref-history/01.ts
@@ -1,4 +1,13 @@
-import { ref } from 'vue';
+import { type Ref, ref } from 'vue';
 
-export function useRefHistory() {
+export function useRefHistory<T>(
+  source: Ref<T>
+) {
+  // const history: Ref<UnwrapRefSimple<T>[], T[] | UnwrapRefSimple<T>[]>
+  // const history = ref<T[]>([]);
+  const history: Ref<T[]> = ref([]);
+  const commit = () => {
+    history.value.push(source.value);
+  };
+  return { history, commit };
 }
